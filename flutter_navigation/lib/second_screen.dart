a87eacdeb9c9b223bdec083a6a92a10cd9093939
lucasgralcantara <lucas.alcantara@cubos.io>
2021-05-20 20:40:45 -0300

    feat: implement secont page

--- flutter_navigation/lib/second_screen.dart
+++ flutter_navigation/lib/second_screen.dart
@@ -3,8 +3,30 @@ import 'package:flutter/material.dart';
 class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Navigator App - Second'),
+      ),
+      body: SafeArea(
+        child: Container(
+          child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              ElevatedButton(
+                  child: Text('Third'),
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'third',
+                        arguments: Colors.orange[100]);
+                  }),
+              ElevatedButton(
+                  child: Text('Fourth'),
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'fourth');
+                  })
+            ]),
+          ),
+        ),
+      ),
     );
   }
 }
