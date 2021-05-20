b36deb6221c0e9ac9e3b04315f7b832d6601135e
lucasgralcantara <lucas.alcantara@cubos.io>
2021-05-20 20:40:51 -0300

    feat: implement third page

--- flutter_navigation/lib/third_screen.dart
+++ flutter_navigation/lib/third_screen.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_navigation/fourth_screen.dart';
 
-class ThirdScreen
- extends StatelessWidget {
+class ThirdScreen extends StatefulWidget {
+  @override
+  _ThirdScreenState createState() => _ThirdScreenState();
+}
+
+class _ThirdScreenState extends State<ThirdScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    Color? incomingBackgroundColor =
+        ModalRoute.of(context)?.settings.arguments as Color?;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Navigator App - Third'),
+      ),
+      body: SafeArea(
+        child: Container(
+          child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              ElevatedButton(
+                  child: Text('Voltar'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
+              ElevatedButton(
+                  child: Text('Quatro removendo'),
+                  onPressed: () {
+                    final parameter = FourthParameters('Lucas', '25');
+                    Navigator.popAndPushNamed(context, 'fourth',
+                        arguments: parameter);
+                  })
+            ]),
+          ),
+        ),
+      ),
+      backgroundColor: incomingBackgroundColor == null
+          ? Colors.black
+          : incomingBackgroundColor,
     );
   }
 }
