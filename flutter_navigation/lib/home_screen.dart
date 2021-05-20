f7fca5369c71b05d1894c21f80b58f741c976a81
lucasgralcantara <lucas.alcantara@cubos.io>
2021-05-20 20:40:56 -0300

    feat: implement home page

--- flutter_navigation/lib/home_screen.dart
+++ flutter_navigation/lib/home_screen.dart
@@ -3,8 +3,29 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Navigator App - First'),
+      ),
+      body: SafeArea(
+        child: Container(
+          child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              ElevatedButton(
+                  child: Text('Second'),
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'second');
+                  }),
+              ElevatedButton(
+                  child: Text('Third'),
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'third');
+                  })
+            ]),
+          ),
+        ),
+      ),
     );
   }
 }
